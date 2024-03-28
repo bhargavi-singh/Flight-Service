012c2e2f23cfb18ed1e7d1b7ae9a1688b23cb731
unknown <bhargavi.singh@criodo.com>
2024-03-28 14:59:43 +0530

    Added basic airpot model and migration without FK constraint

--- src/models/airport.js
+++ src/models/airport.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class City extends Model {
+  class Airport extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,15 +13,29 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  City.init({
+  Airport.init({
     name: {
       type:DataTypes.STRING,
       allowNull:false,
-      unique:true 
+      unique:true
+    },
+    code: {
+      type:DataTypes.STRING,
+      allowNull:false,
+      unique:true
+    },
+    address: {
+      type:DataTypes.STRING,
+      unique:true
+    },
+    cityId: {
+      type:DataTypes.INTEGER,
+      allowNull:false,
+     
     }
   }, {
     sequelize,
-    modelName: 'City',
+    modelName: 'Airport',
   });
-  return City;
+  return Airport;
 };
