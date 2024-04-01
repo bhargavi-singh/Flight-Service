d83f08b395457bf0c4deadd6ab6a4e42e24bc1ca
unknown <bhargavi.singh@criodo.com>
2024-04-01 16:10:16 +0530

    addd

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended:true}))
 app.use('/api',apiRoutes)
 app.listen(ServerConfig.PORT,async()=>{
     console.log("Successfully staarted the server on PORT: " + ServerConfig.PORT)
-    Logger.info("Successfully started the server","root" ,{})
+    // Logger.info("Successfully started the server","root" ,{})
     //  
     // const bengaluru = await Airport.findByPk(1)
     // const bairport = await bengaluru.createAirport({name:'Bengaluru Airport',code:'BLR',cityId:1})
